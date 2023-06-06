06cd8c2ac70415ddea9462efc4a1c62dddc01845
mthflashice <matheuslino@kerix.com.br>
2023-06-06 11:48:27 -0300

    add Migration Data Base, index

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require ('express-async-errors');
 
-const database =require('./database/sqlite');
+const migrationsRun =require('./database/sqlite/migrations');
 
 
 const express = require('express');
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(routes);
 
-database();
+migrationsRun();
 
 app.use((error,request,response, next) =>{
     if(error instanceof appError){
