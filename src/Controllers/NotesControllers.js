dcee763fb63c314e028d9be4a716c6dd66ddd3bc
mthflashice <matheuslino@kerix.com.br>
2023-09-21 10:44:42 -0300

    add details

--- src/Controllers/NotesControllers.js
+++ src/Controllers/NotesControllers.js
@@ -73,7 +73,7 @@ class NotesController{
                 'notes.user_id',
             ])
             .where('notes.user_id', user_id)
-            .whereLike("notes.title", `%${title}%`)
+            .whereLike('notes.title', `%${title}%`)
             .whereIn('name', filterTags)
             .innerJoin('notes', 'notes.id', 'tags.note_id')
             .orderBy('notes.title')
