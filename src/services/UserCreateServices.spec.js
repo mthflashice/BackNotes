e71577e4e126b15d4372c216f344981cb5b0883d
mthflashice <matheuslino@kerix.com.br>
2023-10-23 12:15:30 -0300

    spec email

--- src/services/UserCreateServices.spec.js
+++ src/services/UserCreateServices.spec.js
@@ -1,17 +1,42 @@
 const UserCreateServices= require('./UserCreateServices')
-const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
+const appError = require("../routes/utils/appError");
 
-it ('user should be create',async()=>{
-    const user={
-        name: 'User Test',
-        email: "user@test.com",
-        password: '123'
-    };
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateServices = new UserCreateServices (userRepositoryInMemory);
-    const userCreated = await userCreateServices.execute(user); 
-
-    expect(userCreated).toHaveProperty("id");
+describe('UserCreateServices',()=>{
+    it ('user should be create',async()=>{
+        const user={
+            name: 'User Test',
+            email: "user@test.com",
+            password: '123'
+        };
     
-});
+        const userRepositoryInMemory = new UserRepositoryInMemory();
+        const userCreateServices = new UserCreateServices (userRepositoryInMemory);
+        const userCreated = await userCreateServices.execute(user); 
+    
+        expect(userCreated).toHaveProperty("id");
+        
+    });
+
+    it("user not should be create with exists email", async () => {
+        const user1 = {
+            name: "User Test 1",
+            email: "user@test.com",
+            password: "123"
+        };
+
+        const user2 = {
+            name: "User Test 2",
+            email: "user@test.com",
+            password: "456"
+        };
+
+        const userRepository = new UserRepositoryInMemory();
+        const userCreatedService = new UserCreateServices(userRepository)
+
+        await userCreatedService.execute(user1);
+        await expect(userCreatedService.execute(user2)).rejects.toEqual(new appError(`Este e-mail já é cadastrado `))
+
+    });
+})
